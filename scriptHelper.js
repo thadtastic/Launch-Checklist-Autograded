e72f4727ab3fa619ded9c232cc9db059757a15cd
thadtastic <leothaddeus92@gmail.com>
2023-12-10 03:15:18 -0600

    completed validateInput function  and update for pilot and copilot names

--- scriptHelper.js
+++ scriptHelper.js
@@ -15,30 +15,47 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                  </ol>
                  <img src="">
     */
- }
- 
- function validateInput(testInput) {
-    
- }
- 
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
- 
- async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
- 
-     return planetsReturned;
- }
- 
- function pickPlanet(planets) {
- }
- 
- module.exports.addDestinationInfo = addDestinationInfo;
- module.exports.validateInput = validateInput;
- module.exports.formSubmission = formSubmission;
- module.exports.pickPlanet = pickPlanet; 
- module.exports.myFetch = myFetch;
+}
+
+function validateInput(testInput) {
+    // window.addEventListener("load", function(){
+    //     if(testInput === ""){
+    //         alert("All Fields are required");
+    //     }
+    // })
+   if(testInput === ""){
+     return "Empty";
+   }
+   if(isNaN(testInput)=== false){
+     return "Is a Number";
+   }
+   if(isNaN(testInput)){
+     return "Not a Number";
+   }
+}
+document.getElementById("faultyItems")
+
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+let pilotStatus = document.getElementById("pilotStatus");
+let copilotStatus = document.getElementById("copilotStatus")
+ pilotStatus.innerHTML = `pilot ${pilot} Ready`
+ copilotStatus.innerHTML = `Co-pilot ${copilot} Ready`
+}
+
+async function myFetch() {
+    let planetsReturned;
+
+    planetsReturned = await fetch().then(function (response) {
+    });
+
+    return planetsReturned;
+}
+
+function pickPlanet(planets) {
+}
+
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet;
+module.exports.myFetch = myFetch;

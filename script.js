fdadfaf06dd265b618d6d4da7d7350ddf67d5fdd
thadtastic <leothaddeus92@gmail.com>
2023-12-11 16:44:41 -0600

    Task 2 completed and tested

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 // Write your JavaScript code here!
 
+
+
 window.addEventListener("load", function() {
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event){
+
+        let list = document.getElementById("faultyItems");
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+    
+         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+        event.preventDefault();
     })
+        
+    
+    // parameter values for formsubmission function
+
+//     let listedPlanets;
+//     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+//     let listedPlanetsResponse;
+//    listedPlanetsResponse.then(function (result) {
+//        listedPlanets = result;
+//        console.log(listedPlanets);
+//    }).then(function () {
+//        console.log(listedPlanets);
+//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+//    })
     
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -23,23 +23,60 @@ function validateInput(testInput) {
     //         alert("All Fields are required");
     //     }
     // })
-   if(testInput === ""){
-     return "Empty";
-   }
-   if(isNaN(testInput)=== false){
-     return "Is a Number";
-   }
-   if(isNaN(testInput)){
-     return "Not a Number";
-   }
+    if (testInput === "") {
+        return "Empty";
+    }
+    else if (isNaN(testInput) === false) {
+        return "Is a Number";
+    }
+    else {
+        return "Not a Number";
+    }
 }
-document.getElementById("faultyItems")
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-let pilotStatus = document.getElementById("pilotStatus");
-let copilotStatus = document.getElementById("copilotStatus")
- pilotStatus.innerHTML = `pilot ${pilot} Ready`
- copilotStatus.innerHTML = `Co-pilot ${copilot} Ready`
+
+    //figure how to use validateinput function here???
+    
+    let validatedPilot = validateInput(pilot);
+    let validatedCopilot = validateInput(copilot);
+    let validatedFuellevel = validateInput(fuelLevel);
+    let validatedCargolevel = validateInput(cargoLevel);
+
+    if (validatedPilot === "Empty" || validatedCopilot === "Empty" || validatedFuellevel === "Empty" || validatedCargolevel === "Empty" ){
+        alert("All Fields are Required!");
+    }
+
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
+    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`
+    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`
+
+    let fuelStatus = document.getElementById("fuelStatus");
+    let launchStatus = document.getElementById("launchStatus");
+    if (fuelLevel < 10000) {
+        list.style.visibility = "visible"
+        fuelStatus.innerHTML = `Fuel level too low for launch`
+        launchStatus.innerHTML = `Shuttle Not Ready for Launch`
+        launchStatus.style.color = "red"
+
+    }
+    cargoStatus = document.getElementById("cargoStatus")
+    if (cargoLevel > 10000) {
+        list.style.visibility = "visible"
+        cargoStatus.innerHTML = `Cargo mass too heavy for launch`
+        launchStatus.innerHTML = `Shuttle Not Ready for Launch`
+        launchStatus.style.color = "red"
+    }    
+    if(fuelLevel >= 10000 && cargoLevel <= 10000) {
+        list.style.visibility = "visible"
+        launchStatus.innerHTML = `Shuttle is Ready for Launch`
+        launchStatus.style.color = "green"
+        cargoStatus.innerHTML = "Cargo mass low enough for launch"
+        fuelStatus.innerHTML = "Fuel level high enough for launch"
+
+
+    }
 }
 
 async function myFetch() {
